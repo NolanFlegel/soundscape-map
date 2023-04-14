b08ad029d30a08430558d63ce4b963ab0ad94321
Nolan Flegel <nolanflegel@outlook.com>
2023-04-13 22:13:34 -0700

    Init - default react app

---
+++ map-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+// import logo from './logo.svg';
+import './App.css';
+import HomePage from './components/homePage';
+
+function App() {
+  return (
+    <div className="container">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
